5964abd9aae7f9ec7c0f2d1e2173c455af16e46e
Catsil <imgloriasilva@gmail.com>
2021-11-17 15:41:58 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,92 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
+import Task from "./task.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const INPUT = document.querySelector("input");
+	const [list, setList] = useState([]);
+	const [toDoList, setToDoList] = useState("");
+	// const [failOnUpdating, setFailOnUpdating] = useState("");
+	// const [update, setUpdate] = useState("");
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/gloria", {
+			method: "GET"
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error("Fail");
+				}
+				return response.json();
+			})
+			.then(responseAsJason => {
+				setList(responseAsJason);
+				console.log(responseAsJason);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}, []);
+
+	useEffect(() => {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/gloria", {
+			method: "PUT",
+			body: JSON.stringify(list),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error("fail updating");
+				}
+				return response.json();
+			})
+			.then(responseAsJason => {
+				console.log(responseAsJason);
+			})
+			.catch(error => {
+				console.log(error);
+				// setFailOnUpdating(error.message);
+			});
+	}, [list]);
+
+	useEffect(() => {
+		setToDoList(
+			list.map((task, index) => {
+				return (
+					<Task
+						label={task.label}
+						key={index.toString()}
+						delete={deleteTask}
+						id={index.toString()}
+					/>
+				);
+			})
+		);
+	}, [list]);
+
+	const deleteTask = indexList => {
+		setList(list.filter((_, index) => index != indexList));
+		console.log(indexList);
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<frangement>
+			{/* {failOnUpdating && <h1>{failOnUpdating}</h1>} */}
+			{/* setUpdate()  */}
+			<h1>To do list</h1>
+			<form
+				className="list-wrapper"
+				method="post"
+				onSubmit={event => {
+					event.preventDefault();
+					setList([...list, { label: INPUT.value, done: false }]); // se guarda el imput como diccionario
+					INPUT.value = "";
+				}}>
+				<input type="text" placeholder="Add a task"></input>
+			</form>
+			<ul>{toDoList}</ul>
+		</frangement>
 	);
 };
 
